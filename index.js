04703bc233617d60332f8d71d45b117eb8485713
UmarSarfaraz <89626401+UmarSarfaraz@users.noreply.github.com>
2021-09-01 22:23:29 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./Demo";
+import "./clock.css";
+
+ReactDOM.render(
+  <>
+  <Demo/>
+  </>,
+  document.getElementById("root")
+);
